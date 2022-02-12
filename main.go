3f90f3e070b87880b8e565df794b3c22696e4d0f
afodamz <afodamz07@gmail.com>
2022-02-13 00:29:12 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"strconv"
+	"github.com/gocolly/colly"
+	"fmt"
+	"log"
+	"encoding/json"
+	"os"
+)
+
+// type Article struct {
+// 	Title string `json:"title"`
+// 	Description string `json:"description"`
+// }
+
+type Article struct {
+	ID int `json:"id"`
+	Description string `json:"description"`
+}
+
+func main() {
+	fmt.Println("starting webscraping")
+	articles := make([]Article, 0)
+
+	collector := colly.NewCollector(
+		colly.AllowedDomains("factretriever.com/", "www.lindaikejisblog.com/"),
+	)
+
+	collector.OnHTML(".factsList li", func(element *colly.HTMLElement) {
+		id,err := strconv.Atoi(element.Attr("id"))
+		if err != nil {
+			log.Println("Could not get id")
+		}
+
+		desc := element.Text
+	
+
+		fact := Article{
+			ID: id,
+			Description: desc,
+		}
+
+		articles = append(articles, fact)
+
+		collector.OnRequest(func(req *colly.Request){
+			fmt.Println("Visiting", req.URL.String())
+		})
+		collector.Visit("https://www.factretriever.com/rhino-facts")
+
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent(""," ")
+		enc.Encode(articles)
+	})
+}
